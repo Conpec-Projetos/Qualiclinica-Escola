c63808d06c598bb3725cdcca1f29de20fbe9021b
yellowisk <oheitoralmeida2013@gmail.com>
2025-03-29 01:43:54 -0300

    feat(ButtonQuali): add disabled state and styling

--- src/components/ui/button-quali.tsx
+++ src/components/ui/button-quali.tsx
@@ -2,19 +2,22 @@ interface ButtonProps {
   text: string;
   fontSize?: string;
   onClick?: () => void;
+  disabled?: boolean;
 }
 
 export default function Button({
   text,
   fontSize = "15px",
   onClick,
+  disabled = false,
 }: ButtonProps) {
   return (
     <button
-      className={`border border-transparent bg-[#88C8D4] text-white rounded-[5px]
+      className={`border border-transparent ${disabled ? 'bg-(--menta) text-[#88C8D4]' : 'bg-[#88C8D4] text-white'} rounded-[5px]
                 hover:border-[#88C8D4] hover:bg-transparent hover:text-[#88C8D4]
                 py-1 px-5 transition-all duration-300 text-[${fontSize}] uppercase cursor-pointer`}
       onClick={onClick}
+      disabled={disabled}
     >
       {text}
     </button>
